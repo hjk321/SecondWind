eaee2dbdd279eee47a3d014e031b222da98f03fa
Hiram Anderson <37224753+hjk321@users.noreply.github.com>
2024-06-15 16:00:31 -0600

    Adjust potion effects

--- src/main/kotlin/com/hjk321/secondwind/DyingPlayerHandler.kt
+++ src/main/kotlin/com/hjk321/secondwind/DyingPlayerHandler.kt
@@ -16,20 +16,31 @@ import java.util.*
 class DyingPlayerHandler : Listener {
     private val dyingPlayers: ArrayList<UUID> = ArrayList()
 
-    private fun startDeath(player: Player) {
+    private fun startDying(player: Player) {
         dyingPlayers.add(player.uniqueId)
         player.health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.value // todo don't assert
         player.addPotionEffect(PotionEffect(
-            PotionEffectType.WITHER,
-            PotionEffect.INFINITE_DURATION, 1)
+            PotionEffectType.WITHER, PotionEffect.INFINITE_DURATION,
+            1, false, false, false)
         )
         player.addPotionEffect(PotionEffect(
-                PotionEffectType.SLOWNESS,
-            PotionEffect.INFINITE_DURATION, 4)
+            PotionEffectType.SLOWNESS, PotionEffect.INFINITE_DURATION,
+            4, false, false, false)
+        )
+        player.addPotionEffect(PotionEffect(
+            PotionEffectType.MINING_FATIGUE, PotionEffect.INFINITE_DURATION,
+            2, false, false, false)
         )
         player.setPose(Pose.SWIMMING, true)
-        // Hacky way to make player crawl client-side
-        // player.sendBlockChange(player.location.add(0.0, 1.0, 0.0), Material.BARRIER.createBlockData())
+    }
+
+    private fun secondWind(player: Player) {
+        player.removePotionEffect(PotionEffectType.WITHER)
+        player.removePotionEffect(PotionEffectType.SLOWNESS)
+        player.removePotionEffect(PotionEffectType.MINING_FATIGUE)
+        // TODO remove other bad effects
+        player.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION,
+            100, 0, false, false, false))
     }
 
     @EventHandler(priority = EventPriority.HIGH)
@@ -39,7 +50,7 @@ class DyingPlayerHandler : Listener {
             val player = event.entity as Player
             if (event.damage >= player.health && !dyingPlayers.contains(player.uniqueId)) { // TODO more robust check
                 event.damage = 0.0
-                startDeath(player)
+                startDying(player)
             }
         }
     }
